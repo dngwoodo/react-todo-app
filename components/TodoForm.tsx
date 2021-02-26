2e0ef7b3417130145615a8c7d4a43f6faade3958
dngwoodo <dngwoodo@gmail.com>
2021-02-27 02:57:21 +0900

    feat: TodoForm 생성

--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const TodoForm = () => (
-  <div />
-);
+const TodoForm = () => {
+  const [text, setText] = useState('');
+  return (
+    <form onSubmit={
+      (e) => {
+        e.preventDefault();
+        setText('');
+      }
+    }
+    >
+      <input placeholder="할 일을 입력하세요" value={text} onChange={(e) => setText(e.target.value)} />
+      <button type="submit">등록</button>
+    </form>
+  );
+};
 
 export default TodoForm;

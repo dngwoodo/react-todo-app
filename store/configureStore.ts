c2375a66acc327bb42d0e5f3d1fd9170ca91cbef
dngwoodo <dngwoodo@gmail.com>
2021-03-01 13:27:20 +0900

    chore: redux toolkit 셋팅

---
+++ store/configureStore.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import rootReducer from '../slices';
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+type RootState = ReturnType<typeof rootReducer>
+type AppDispatch = typeof store.dispatch
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

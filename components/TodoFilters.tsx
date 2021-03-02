82dda186f3b6fb8d65346679c29aea622eef60fc
dngwoodo <dngwoodo@gmail.com>
2021-03-03 00:27:31 +0900

    feat: TodoFilter 생성

--- components/TodoFilters.tsx
+++ components/TodoFilters.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+const filters = [
+  { text: '전체', filter: 'ALL' },
+  { text: '완료', filter: 'DONE' },
+  { text: '미완료', filter: 'UNDONE' },
+];
+
 const TodoFilters = () => (
-  <div />
+  <div>
+    {filters.map(({ text, filter }) => (
+      <button
+        type="button"
+        key={filter}
+      >
+        {text}
+      </button>
+    ))}
+  </div>
 );
 
 export default TodoFilters;

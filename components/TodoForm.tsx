6f081d2b447897dfe00fae3fdbac766f0e9efa0b
dngwoodo <dngwoodo@gmail.com>
2021-03-07 00:00:11 +0900

    refactor: TodoForm.tsx에서 usecallback deps 변경

--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -6,11 +6,10 @@ const TodoForm = () => {
   const [text, setText] = useState('');
 
   // 함수형 컴포넌트를 사용하면 리랜더링 될때 이 컴포넌트 전체가 다시 실행되므로 useCallback을 써서 메모리에 올려두고 memoization(캐싱)한다.
-  // deps에 text를 넣어주는 이유는 함수가 리랜더링되었을 때 text가 바꼈으면 새로운 함수를 반환하고 아니면 메모리에 올려둔 함수를 반환하기 위해서이다.
   // 또한 usecallback은 자식에게 넘겨주는 함수일 경우에는 반드시 해주는 것이 좋다. 여기서 리랜더링 되면 자식도 리랜더링 되기버리기 때문이다.
   const inputOnChange = useCallback((e) => {
     setText(e.target.value);
-  }, [text]); // text를 안넣어주면 리랜더링 시 값을 참조할때 최신값을 받아온다고 보장할 수 없음
+  }, []);
 
   // eslint-disable-next-line no-unused-vars
   const [_, { add }] = useTodos();
